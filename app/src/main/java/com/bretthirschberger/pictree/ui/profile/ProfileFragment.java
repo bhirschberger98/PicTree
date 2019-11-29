c3a1e5b382729f35a1b456e6fa51839b30c28435
Brett Hirschberger <bretthirschberger@gmail.com>
2019-11-29 02:23:33 -0500

    added post fragment

--- app/src/main/java/com/bretthirschberger/pictree/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/bretthirschberger/pictree/ui/profile/ProfileFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.TabHost;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -17,19 +17,44 @@ import com.bretthirschberger.pictree.R;
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel mProfileViewModel;
+//    private TabLayout mTabLayout;
+//    private TabItem mRootTab;
+//    private TabItem mBranchTab;
+    private TabHost mTabHost;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mProfileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+        mTabHost =root.findViewById(R.id.profile_tabs);
+        mTabHost.setup();
+
+        TabHost.TabSpec spec = mTabHost.newTabSpec("Branches");
+        spec.setContent(R.id.profile_branches_tab);
+        spec.setIndicator(getResources().getString(R.string.profile_branches));
+        mTabHost.addTab(spec);
+
+        spec = mTabHost.newTabSpec("Roots");
+        spec.setContent(R.id.profile_roots_tab);
+        spec.setIndicator(getResources().getString(R.string.profile_roots));
+        mTabHost.addTab(spec);
+
         mProfileViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+//                textView.setText(s);
             }
         });
+
         return root;
+    }
+
+    public void populateBranches(View view) {
+
+    }
+
+    public void populateRoots(View view) {
+
     }
 }
